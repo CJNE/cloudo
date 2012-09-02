e22ba58d0cc7d7e7eaba355c8748775bae269e6e
Johan Isacsson <johan@oddjob.se>
2012-09-03 00:41:20 +0200

    Initial version

---
+++ ec2.rb
@@ -0,0 +1,38 @@
+#!/usr/bin/env ruby
+#
+require 'rubygems'
+require 'fog'
+require 'active_support'
+
+class Ec2Command
+    def initialize()
+        @provider= Fog::Compute.new(:provider => 'AWS')
+        @servers = ARGV[1].nil? ? @provider.servers : @provider.servers.all('tag:Name' => ARGV[1])
+    end
+    def method_missing(m, *args, &block)
+        puts "There's no method called #{m} here -- please try again."
+    end
+    def _find(name)
+        @servers.select {|s| s.tags["Name"].include? name}
+    end
+    def ssh
+        server = @servers[0]
+        system("ssh -i ~/.ssh/#{server.key_name} #{ARGV[2]}@#{server.public_ip_address}")
+    end
+    def show
+        puts @servers[0].inspect
+    end
+    def find
+        server = _find(ARGV[1])[0]
+        puts server.inspect
+    end
+    def list
+        @servers.each do |server| 
+            name = server.tags["Name"] || server.id
+            puts name
+        end
+    end
+end
+command = ARGV[0]
+instance = Ec2Command.new
+instance.send(command)
