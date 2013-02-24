56bee6f15ded506fcc6bfb85544587249f412320
Johan Isacsson <johan@generatorhallen.se>
2013-02-24 23:40:05 +0100

    Added support for password authentication and an option to specify the path to the ssh key

--- lib/cloudo/ssh.rb
+++ lib/cloudo/ssh.rb
@@ -3,9 +3,20 @@ module Cloudo
     def initialize(args, options)
       @base_options = options
       @banner = "Usage: cloudo ssh [options] [user@]identifier"
+      @ssh_options = {
+        :use_password => false,
+        :key => ''
+      }
       parse_args args do |opts|
-        opts.on('-t', '--tmux') do
-          @options[:tmux] = true
+        opts.on('-k', '--key KEY',
+               'Path to the ssh private key to use, defaults to the server key name attribute') do |key|
+          @ssh_options[:key] = key
+        end
+        opts.on('-p', '--password', "Use password authentication") do
+          @ssh_options[:use_password] = true
+        end
+        opts.on('-t', '--tmux', "Start session in a new tmux buffer") do
+          @ssh_options[:tmux] = true
         end
       end
       @instance_id = args[1]
@@ -21,8 +32,14 @@ module Cloudo
 
     def execute(provider)
       server = get_instance provider, @instance_id 
-      exe = "ssh -i ~/.ssh/#{server.key_name} #{@user}#{server.public_ip_address}"
-      # puts exe
+      exe = "ssh "
+      if @ssh_options[:key].length > 0
+        exe << "-i #{@ssh_options[:key]} "
+      elsif !@ssh_options[:use_password]
+        puts "nay"
+        exe << "-i | '.ssh/'+server.key_name} "
+      end
+      exe << "#{@user}#{server.public_ip_address}"
       system exe
     end
   end
